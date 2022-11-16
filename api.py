fc1279be1cc8fc20f055a7a65eecf49eff1b2abc
GeekSloth <gothsleek@gmail.com>
2022-11-17 02:36:39 +0700

    first success

--- api.py
+++ api.py
@@ -1,128 +1,152 @@
-import os
+#import os
 import argparse
 from time import time
 import logging as log
 import cv2
+import torch
 import numpy as np
 import json
-import requests
-import queue
-import threading
-from flask import Flask, request, redirect, render_template
+#import requests
+#import queue
+#import threading
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 from PIL import Image
-import base64
-from io import BytesIO
+#import base64
+#from io import BytesIO
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from collections import defaultdict
+from pathlib import Path
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+	scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
-tic = time()
 auth = HTTPBasicAuth()
 users = {
-    "a": generate_password_hash("0202")
+	"a": generate_password_hash("1234")
 }
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+	# Resize and pad image while meeting stride-multiple constraints
+	shape = img.shape[:2]  # current shape [height, width]
+	if isinstance(new_shape, int):
+		new_shape = (new_shape, new_shape)
 
+	# Scale ratio (new / old)
+	r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+	if not scaleup:  # only scale down, do not scale up (for better test mAP)
+		r = min(r, 1.0)
+
+	# Compute padding
+	ratio = r, r  # width, height ratios
+	new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+	dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+	if auto:  # minimum rectangle
+		dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+	elif scaleFill:  # stretch
+		dw, dh = 0.0, 0.0
+		new_unpad = (new_shape[1], new_shape[0])
+		ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+	dw /= 2  # divide padding into 2 sides
+	dh /= 2
+
+	if shape[::-1] != new_unpad:  # resize
+		img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+	top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+	left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+	img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+	return img, ratio, (dw, dh)
 def check_allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_exts
-
 def pprint(mydict):
 	print(json.dumps(mydict, sort_keys=True, indent=4))
-
-class VideoCapture2:
-	def __init__(self, name):
-		self.cap = cv2.VideoCapture(name)
-		self.q = queue.Queue()
-		t = threading.Thread(target=self._reader)
-		t.daemon = True
-		t.start()
-	def _reader(self):
-		while True:
-			ret, frame = self.cap.read()
-			if not ret:
-				break
-			if not self.q.empty():
-				try:
-					self.q.get_nowait()
-				except queue.Empty:
-					pass
-			self.q.put(frame)
-	def read(self):
-		return self.q.get()
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-	"-c", "--confidence", type=float, default=0.05,	help="confidence threshold"
-)
-parser.add_argument(
-	"-nms", "--non_maximum", type=float, default=0.5, help="non-maximum supression threshold"
-)
-parser.add_argument(
-	"-ll", "--logging_level", type=str, default=30, help="logging level"
-)
-parser.add_argument(
-	"-r", "--render", type=bool, default=False, help="return with base64 image and rendering"
-)
-args = vars(parser.parse_args())
-log.basicConfig(level=args["logging_level"])
-os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
-classes = list()
-with open("resources/labels.txt", "r") as f:
-	classes = [cname.strip() for cname in f.readlines()]
-NN = cv2.dnn.readNetFromDarknet(
-	"resources/yolov4-tiny.cfg",
-	"resources/yolov4-tiny.weights"
-)
-NN.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-NN.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-NN_layer = NN.getLayerNames()
-NN_layer = [NN_layer[i - 1] for i in NN.getUnconnectedOutLayers()]
-cv2.cuda.setDevice(0)
 allowed_exts = {'jpg', 'jpeg','png','JPG','JPEG','PNG'}
 app = Flask(__name__)
-log.info("initialized [{:.4f} ms]".format(time()-tic))
-log.info("ready")
+#### integration begin...
+parser = argparse.ArgumentParser()
+parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+parser.add_argument('--source', type=str, default='inference/images/bus.jpg', help='source')  # file/folder, 0 for webcam
+parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+parser.add_argument('--view-img', action='store_true', help='display results')
+parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+parser.add_argument('--augment', action='store_true', help='augmented inference')
+parser.add_argument('--update', action='store_true', help='update all models')
+parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+parser.add_argument('--name', default='exp', help='save results to project/name')
+parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+opt = parser.parse_args()
+#print(opt)
+source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+#webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+#	('rtsp://', 'rtmp://', 'http://', 'https://'))
+# Directories
+#save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+#(save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
+# Initialize
+#set_logging()
+device = select_device(opt.device)
+#half = device.type != 'cpu'  # half precision only supported on CUDA
+half = False
+
+# Load model
+model = attempt_load(weights, map_location=device)  # load FP32 model
+#stride = int(model.stride.max())  # model stride
+#imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+#if trace:
+#	model = TracedModel(model, device, opt.img_size)
+
+# Second-stage classifier
+#classify = False
+#if classify:
+#	modelc = load_classifier(name='resnet101', n=2)  # initialize
+#	modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+# Set Dataloader
+#vid_path, vid_writer = None, None
+#if webcam:
+#	view_img = check_imshow()
+#	cudnn.benchmark = True  # set True to speed up constant image size inference
+#	dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+#else:
+#dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+# Get names and colors
+names = model.module.names if hasattr(model, 'module') else model.names
+#print(names[0])
+colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in names]
+
+# Run inference
+#if device.type != 'cpu':
+#	model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+#old_img_w = old_img_h = imgsz
+#old_img_b = 1
+
+#t0 = time()
+
+
+
+
+#### ...end integration
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
-        return username
-
-@app.route('/config', methods=['GET', 'POST'])
-@auth.login_required
-def config():
-	if request.method == 'POST':
-		data = request.json
-		if not (data.get('confidence') is None):
-			args["confidence"] = data.get('confidence')
-			log.debug("confidence: {}".format(args["confidence"]))
-		if not (data.get('non_maximum') is None):
-			args["non_maximum"] = data.get('non_maximum')
-			log.debug("non_maximum: {}".format(args["non_maximum"]))
-		if not (data.get('logging_level') is None):
-			args["logging_level"] = data.get('logging_level')
-			log.getLogger().setLevel(data.get('logging_level'))
-			log.debug("logging_level: {}".format(args["logging_level"]))
-		if not (data.get('render') is None):
-			args["render"] = data.get('render')
-			log.debug("render: {}".format(args["render"]))
-		return "configured!", 200
-	if request.method == 'GET':
-		return '''
-			<a href="/" alt="home">home</a> / <a href="/config" alt="home">config</a> <br />
-			<h1>POST configurations</h1>
-			<h3> available parameters: </h3>
-			confidence [{}] <br />
-			non_maximum [{}] <br />
-			logging_level [{}] <br />
-			render [{}]
-		'''.format(
-			args["confidence"],
-			args["non_maximum"],
-			args["logging_level"],
-			args["render"]
-			), 200
+	if username in users and check_password_hash(users.get(username), password):
+		return username
 
 @app.route("/",methods=['GET', 'POST'])
 @auth.login_required
@@ -130,75 +154,144 @@ def index():
 	if request.method == 'POST':
 		if 'file' not in request.files:
 			print('No file attached in request')
-			return redirect(request.url)
+			#return redirect(request.url)
+			return "No file attached", 200
 		file = request.files['file']
 		if file.filename == '':
 			print('No file selected')
-			return redirect(request.url)
+			#return redirect(request.url)
+			return "No file selected", 200
 		if file and check_allowed_file(file.filename):
+			t0 = time()
 			payload = defaultdict(list)
-			boxes = []
-			classIds = []
-			confidences = []
-			tic = time()
-			tic_origin = tic
 			filename = secure_filename(file.filename)
 			img = Image.open(file.stream)
-			if img.mode != 'RGB':
-				img = img.convert('RGB')
-			open_cv_image = np.array(img) 
-			frame = open_cv_image[:, :, ::-1].copy() 
-			W, H = img.size
-			payload["stats"].append(["pre-process (ms)", round((time()-tic)*1000)])
-			tic = time()
-			NN.setInput(
-				cv2.dnn.blobFromImage(
-					frame, 
-					1/255.0, (416, 416),
-					swapRB=True, 
-					crop=False
-				)
-			)
-			for output in NN.forward(NN_layer):
-				for detection in output:
-					scores = detection[5:]
-					classID = np.argmax(scores)
-					confidence = scores[classID]
-					if confidence > args["confidence"]:
-						box = detection[0:4] * np.array([W, H, W, H])
-						(center_x, center_y, width, height) = box.astype("int")
-						x = int(center_x - (width/2))
-						y = int(center_y - (height/2))
-						boxes.append([x, y, int(width), int(height)])
-						classIds.append(classID)
-						confidences.append(float(confidence))
-			payload["stats"].append(["detect (ms)", round((time()-tic)*1000)])
-			tic = time()
-			idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["non_maximum"])
-			if len(idxs) > 0:
-				for i in idxs.flatten():
-					(x, y) = (boxes[i][0], boxes[i][1])
-					(w, h) = (boxes[i][2], boxes[i][3])
-					obj = ["{}".format(classes[classIds[i]]), [x,y,w,h], round(confidences[i], 4)]
-					payload["detected"].append(obj)
-					cv2.putText(frame, "{}".format(classes[classIds[i]]), (x+(int)(w/2), y+(int)(h/2)),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-			retval, buffer = cv2.imencode('.jpg', frame)
-			encoded_string = base64.b64encode(buffer).decode() 
-			toc = time()
-			payload["stats"].append(["post-process (ms)", round((toc-tic)*1000)])
-			payload["stats"].append(["total (ms)", round((toc-tic_origin)*1000)])
+			#print(type(img))
+			#if img.mode != 'RGB':
+			#	img = img.convert('RGB')
+			open_cv_image = np.array(img)
+			frame_0 = open_cv_image
+			frame = open_cv_image
+			#frame = open_cv_image[:, :, ::-1].copy() 
+			#frame = open_cv_image.T
+			#frame = LoadImages(file.read())
+			#W, H = img.size
+			#### integration begin...
+			frame = letterbox(frame)[0]
+			frame = frame[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+			frame = np.ascontiguousarray(frame)
+			print(type(frame))
+			print(frame.dtype)
+			print(frame.size)
+			print(frame.ndim)
+			print(frame.shape)
+			frame = torch.from_numpy(frame).to(device)
+			frame = frame.half() if half else frame.float()  # uint8 to fp16/32
+			#frame = frame.float()
+			frame /= 255.0  # 0 - 255 to 0.0 - 1.0
+			if frame.ndimension() == 3:
+				frame = frame.unsqueeze(0)
+
+			# Warmup
+			
+			if device.type != 'cpu' and True:
+				old_img_b = frame.shape[0]
+				old_img_h = frame.shape[2]
+				old_img_w = frame.shape[3]
+				for i in range(3):
+					model(frame, augment=opt.augment)[0]
+			
+
+			# Inference
+			t1 = time_synchronized()
+			with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+				pred = model(frame, augment=opt.augment)[0]
+			t2 = time_synchronized()
+
+			# Apply NMS
+			pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+			t3 = time_synchronized()
+
+			# Apply Classifier
+			#if classify:
+			#	pred = apply_classifier(pred, modelc, img, im0s)
+
+			# Process detections
+			for i, det in enumerate(pred):  # detections per image
+				#print(det)
+				#if webcam:  # batch_size >= 1
+				#	p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+				#else:
+				'''
+				p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+				#p = Path(p)  # to Path
+				#save_path = str(save_dir / p.name)  # img.jpg
+				#txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+				'''
+				s = ''
+				gn = torch.tensor(frame_0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+				if len(det):
+					# Rescale boxes from img_size to im0 size
+					det[:, :4] = scale_coords(frame.shape[2:], det[:, :4], frame_0.shape).round()
+
+					# Print results
+					for c in det[:, -1].unique():
+						n = (det[:, -1] == c).sum()  # detections per class
+						s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+					# Write results
+					for *xyxy, conf, cls in reversed(det):
+						#xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh --original
+						xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()
+						xywh = [int(xywh) for xywh in xywh]
+						#print(xywh)
+						line = (names[int(cls.tolist())], xywh, round(conf.tolist(), 4))# if opt.save_conf else (cls, *xywh)  # label format
+						#print(line)
+						payload["detected"].append(line)
+						'''
+						if save_txt:  # Write to file
+							xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+							line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+							with open(txt_path + '.txt', 'a') as f:
+								f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+						if save_img or view_img:  # Add bbox to image
+							label = f'{names[int(cls)]} {conf:.2f}'
+							plot_one_box(xyxy, frame_0, label=label, color=colors[int(cls)], line_thickness=1)
+						'''
+				
+				# Print time (inference + NMS)
+				payload["info"].append(["summary", (f'{s}Done. Inference: {(1E3 * (t2 - t1)):.1f}ms, NMS: {(1E3 * (t3 - t2)):.1f}ms')])
+
+				# Stream results
+				#if view_img:
+				#	cv2.imshow(str(p), im0)
+				#	cv2.waitKey(1)  # 1 millisecond
+
+				# Save results (image with detections)
+				
+				#if dataset.mode == 'image':
+				#	cv2.imwrite(save_path, im0)
+				#	print(f" The image with the result is saved in: {save_path}")
+					
+			#if save_txt or save_img:
+			#	s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+				#print(f"Results saved to {save_dir}{s}")
+
+			payload["info"].append(["total", (f'{time() - t0:.3f}s')])
+
+
+			#### ... end integration
+			#encoded_string = base64.b64encode(buffer).decode() 
 		response = json.dumps({
 			"detected": payload["detected"],
-			"stats": payload["stats"]
+			"info": payload["info"]
 			})
-		if args["render"]:
-			return render_template('index.html', img_data=encoded_string, json_data=response), 200
-		else:
-			return response, 200
+		return response, 200
 	else:
-		return render_template('index.html', img_data="", json_data=""), 200
+		return "OK", 200
 
 if __name__ == "__main__":
 	app.debug=True
-	app.run(host='0.0.0.0', port=5002)
+	app.run(host='0.0.0.0', port=5003)
